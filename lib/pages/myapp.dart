1d8ada9588bc19e7f021d1afcd20740e373deae3
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-05-07 16:25:23 -0300

    Separate-widget-myapp to main

---
+++ lib/pages/myapp.dart
@@ -0,0 +1,14 @@
+import 'package:day_counters/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const LoginPage(),
+    );
+  }
+}
